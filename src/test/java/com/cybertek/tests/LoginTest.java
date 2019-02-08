7275a44bbb4eb07267cffa6202da0ed0d8c7d001
mesutduman8991 <mesutduman8991@gmail.com>
2019-02-08 18:41:59 -0500

    add LoginPage and commert'

--- src/test/java/com/cybertek/tests/LoginTest.java
+++ src/test/java/com/cybertek/tests/LoginTest.java
@@ -12,7 +12,8 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
-
+// login tests are maintained here.
+// without Page object approach
 public class LoginTest {
 
     WebDriver driver;
